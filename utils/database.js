2116e6980f6b1cb7af6a1486ff4377561453fac9
Jaspreet Singh <js9316713287@gmail.com>
2024-10-09 11:15:09 +0530

    Initial Commit

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const databaseConnection = async() => {
     try {
-        await mongoose.connect('mongodb+srv://healthydaycorporations:HealthyDayCorporations@healthydaycluster.0w1cs.mongodb.net/');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log(`Database Connected Successfully`);
     } catch (error) {
         console.log(`Failed to Connect to database : ${error.message}`)
